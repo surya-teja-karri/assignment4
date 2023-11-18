a93e54a5e8ab3392fd9b1fee58b14be217227602
JeevikaSuresh <145818325+JeevikaSuresh@users.noreply.github.com>
2023-11-18 10:56:45 -0500

    Update ImageToImage.py

--- ImageToImage.py
+++ ImageToImage.py
@@ -12,7 +12,24 @@ import matplotlib.pyplot as plt
 from sklearn import manifold
 from matplotlib import offsetbox
 
+# Load your code for style embeddings and searching
 
+def load_image(image):
+    image = plt.imread(image)
+    img = tf.image.convert_image_dtype(image, tf.float32)
+    img = tf.image.resize(img, [400, 400])
+    img = img[tf.newaxis, :]
+    return img
+
+content_layers = ['block5_conv2']
+
+style_layers = [
+    'block1_conv1',
+    'block2_conv1',
+    'block3_conv1',
+    # 'block4_conv1',
+    # 'block5_conv1'
+]
 
 
 
